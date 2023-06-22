60085943d7be49ac9c7c1b93d3d36f07c87471af
Dan G <daniel.w.gray1994@gmail.com>
2023-06-22 16:21:35 -0400

    added 2 new functions to handle the last two options in my switch case

--- index.js
+++ index.js
@@ -69,18 +69,89 @@ const addRole = () => {
       message: "What is the salary for this role?",
       name: "roleSalary",
     },
+    {
+      type: "input",
+      message: "What is the department id for this role?",
+      name: "department_id",
+    },
   ];
 
-  inquirer.prompt(questions).then((answer) => {
-    console.log(answer);
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
     db.query(
-      `INSERT INTO role (name) VALUES (?, ?, ?)`,
-      (answer.roleName, answer.roleSalary, answer.department_id),
+      `INSERT INTO role (title, salary, department_id) VALUES ("${answers.roleName}", ${answers.roleSalary}, ${answers.department_id})`,
       (err, result) => {
         if (err) {
           console.log(err);
         }
         rolesQuery();
+      }
+    );
+  });
+};
+
+// query to add employee
+const addEmployee = () => {
+  const questions = [
+    {
+      type: "input",
+      message: "What is the first name of the new employee?",
+      name: "first_name",
+    },
+    {
+      type: "input",
+      message: "What is the last name of the new employee?",
+      name: "last_name",
+    },
+    {
+      type: "input",
+      message: "What is the role id for this employee?",
+      name: "role_id",
+    },
+    {
+      type: "input",
+      message: "What is the manager id for this employee?",
+      name: "manager_id",
+    },
+  ];
+
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    db.query(
+      `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answers.first_name}", "${answers.last_name}", ${answers.role_id}, ${answers.manager_id})`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        employeesQuery();
+      }
+    );
+  });
+};
+
+const updateEmployeeRole = () => {
+  const questions = [
+    {
+      type: "input",
+      message: "What is the id of the employee?",
+      name: "employee_id",
+    },
+    {
+      type: "input",
+      message: "What is the new role id for the employee?",
+      name: "role_id",
+    },
+  ];
+
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    db.query(
+      `UPDATE employee SET role_id = ${answers.role_id} WHERE id = ${answers.employee_id}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        employeesQuery();
       }
     );
   });
@@ -95,7 +166,7 @@ const initQuestion = {
     "View all employees",
     "Add a department",
     "Add a role",
-    "Add an emplopyee",
+    "Add an employee",
     "Update an employee role",
   ],
   name: "firstChoice",
@@ -121,6 +192,12 @@ function handleAnswers(answers) {
       break;
     case "Add a role":
       addRole();
+      break;
+    case "Add an employee":
+      addEmployee();
+      break;
+    case "Update an employee role":
+      updateEmployeeRole();
       break;
   }
 }
